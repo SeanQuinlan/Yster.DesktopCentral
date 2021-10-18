e7a40eda607cefd331a021d6e8ad4052cbacbccf
Sean Quinlan <github@yster.org>
2021-10-18 18:25:40 +0100

    Some updates to support custom groups. Made error return a bit more generic

--- Source/Private/Invoke-DCQuery.ps1
+++ Source/Private/Invoke-DCQuery.ps1
@@ -22,29 +22,17 @@ function Invoke-DCQuery {
 
     [CmdletBinding()]
     param(
-        # The hostname of the Desktop Central server.
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $HostName,
-
-        # The port of the Desktop Central server.
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [Int]
-        $Port,
-
         # The API path.
         [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [String]
         $APIPath,
 
-        # The REST Method to use.
-        [Parameter(Mandatory = $true)]
-        [ValidateSet('GET', 'POST')]
+        # The AuthToken for the API.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
         [String]
-        $Method,
+        $AuthToken,
 
         # The message body.
         [Parameter(Mandatory = $false)]
@@ -52,21 +40,50 @@ function Invoke-DCQuery {
         [Hashtable]
         $Body,
 
-        # The AuthToken for the API.
+        # Additional header to add.
         [Parameter(Mandatory = $false)]
         [ValidateNotNullOrEmpty()]
+        [Hashtable]
+        $Header,
+
+        # The hostname of the Desktop Central server.
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
         [String]
-        $AuthToken
+        $HostName,
+
+        # The REST Method to use.
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('GET', 'POST')]
+        [String]
+        $Method,
+
+        # The port of the Desktop Central server.
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Int]
+        $Port
     )
 
     $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
-    $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' ')) }
+    $PSBoundParameters.GetEnumerator() | ForEach-Object {
+        if ($_.Value -is [Hashtable]) {
+            Write-Verbose ("{0}|Arguments: {1}:`n{2}" -f $Function_Name, $_.Key, ($_.Value | Format-Table -AutoSize | Out-String).Trim())
+        } else {
+            Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' '))
+        }
+    }
 
-    $API_Version = '1.3'
-    $API_Uri = 'http://{0}:{1}/api/{2}/{3}' -f $HostName, $Port, $API_Version, $APIPath
+    if ($APIPath -match '^dcapi') {
+        $API_Uri = 'http://{0}:{1}/{2}' -f $HostName, $Port, $APIPath
+        $NewAPI = $true
+    } else {
+        $API_Version = '1.3'
+        $API_Uri = 'http://{0}:{1}/api/{2}/{3}' -f $HostName, $Port, $API_Version, $APIPath
+    }
 
     try {
-        $API_Parameters = @{
+        $global:API_Parameters = @{
             'Uri'         = $API_Uri
             'Method'      = $Method
             'ContentType' = 'application/json'
@@ -79,29 +96,39 @@ function Invoke-DCQuery {
                 'Authorization' = $AuthToken
             }
         }
+        if ($PSBoundParameters.ContainsKey('Header')) {
+            $API_Parameters['Header'] += $Header
+        }
 
         try {
-            $REST_Response = Invoke-RestMethod @API_Parameters
-            Write-Verbose ('{0}|Response status: {1}' -f $Function_Name, $REST_Response.status)
+            $global:REST_Response = Invoke-RestMethod @API_Parameters
 
-            switch ($REST_Response.status) {
-                'error' {
-                    $Terminating_ErrorRecord_Parameters = @{
-                        'Exception'    = 'System.InvalidOperationException'
-                        'ID'           = 'DC-REST-Error-{0}' -f $REST_Response.error_code
-                        'Category'     = 'InvalidResult'
-                        'TargetObject' = $API_Uri
-                        'Message'      = $REST_Response.error_description
+            if ($NewAPI) {
+                # The new API (host:port/dcapi) returns the objects directly, rather than as a sub-property
+                Write-Verbose ('{0}|Response items returned: {1}' -f $Function_Name, $REST_Response.Count)
+                $Return_Object = $REST_Response
+            } else {
+                Write-Verbose ('{0}|Response status: {1}' -f $Function_Name, $REST_Response.status)
+                switch ($REST_Response.status) {
+                    'error' {
+                        $Terminating_ErrorRecord_Parameters = @{
+                            'Exception'    = 'System.InvalidOperationException'
+                            'ID'           = 'DC-REST-Error-{0}' -f $REST_Response.error_code
+                            'Category'     = 'InvalidResult'
+                            'TargetObject' = $API_Uri
+                            'Message'      = $REST_Response.error_description
+                        }
+                        $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
+                        $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
                     }
-                    $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
-                    $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
-                }
-                'success' {
-                    $Message_Type = $REST_Response.message_type
-                    # Return the relevant message_response child object
-                    $REST_Response.message_response.$Message_Type | Add-CalculatedTime | ConvertTo-SortedPSObject
+                    'success' {
+                        $Message_Type = $REST_Response.message_type
+                        # Return the relevant message_response child object
+                        $Return_Object = $REST_Response.message_response.$Message_Type
+                    }
                 }
             }
+            $Return_Object | Add-CalculatedProperty | ConvertTo-SortedPSObject
         } catch [System.Net.WebException] {
             if ($_.ErrorDetails.Message) {
                 $Returned_ErrorDetails = $_.ErrorDetails.Message | ConvertFrom-Json
@@ -131,7 +158,15 @@ function Invoke-DCQuery {
                     $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
                 }
                 default {
-                    throw $_
+                    $Terminating_ErrorRecord_Parameters = @{
+                        'Exception'    = 'System.Net.WebException'
+                        'ID'           = 'DC-REST-Error-{0}' -f $REST_Response.error_code
+                        'Category'     = 'InvalidResult'
+                        'TargetObject' = $API_Uri
+                        'Message'      = $Returned_ErrorDetails.ErrorMsg
+                    }
+                    $Terminating_ErrorRecord = New-ErrorRecord @Terminating_ErrorRecord_Parameters
+                    $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
                 }
             }
         }
