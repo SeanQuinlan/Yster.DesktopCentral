2d00571913627866968cd1b6d3bf1357f257bb77
Sean Quinlan <github@yster.org>
2021-10-19 16:17:28 +0100

    Readded Get-DCConfiguration after some further testing

---
+++ Source/Public/Get-DCConfiguration.ps1
@@ -0,0 +1,78 @@
+function Get-DCConfiguration {
+    <#
+    .SYNOPSIS
+        Gets a list of configurations from the server.
+    .DESCRIPTION
+        Returns all configurations available on the server, or filtered by domain, branch office or status.
+    .EXAMPLE
+        Get-DCConfiguration -HostName DCSERVER -AuthToken '47A1157A-7AAC-4660-XXXX-34858F3A001C'
+
+        Returns all the configurations in the environment.
+    .NOTES
+        https://www.manageengine.com/patch-management/api/view-configuration-patch-management.html
+    #>
+
+    [CmdletBinding()]
+    param(
+        # The AuthToken for the Desktop Central server API.
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $AuthToken,
+
+        # The Branch Office to filter on.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $BranchOffice,
+
+        # The Configuration Status to filter on.
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('InProgress', 'Draft', 'Executed', 'Suspended', 'Deployed', 'RetryInProgress', 'Expired')]
+        [String]
+        $ConfigStatus,
+
+        # The NETBIOS name of the Domain to filter on.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Domain,
+
+        # The hostname of the Desktop Central server.
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $HostName,
+
+        # The port of the Desktop Central server.
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [Int]
+        $Port = 8020
+    )
+
+    $Function_Name = (Get-Variable MyInvocation -Scope 0).Value.MyCommand.Name
+    $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose ('{0}|Arguments: {1} - {2}' -f $Function_Name, $_.Key, ($_.Value -join ' ')) }
+
+    try {
+        $API_Path = Add-Filters -BoundParameters $PSBoundParameters -BaseURL 'patch/viewconfig'
+        $Query_Parameters = @{
+            'AuthToken' = $AuthToken
+            'HostName'  = $HostName
+            'Port'      = $Port
+            'APIPath'   = $API_Path
+            'Method'    = 'GET'
+        }
+        Write-Verbose ('{0}|Calling Invoke-DCQuery' -f $Function_Name)
+        $Query_Return = Invoke-DCQuery @Query_Parameters
+        $Query_Return
+
+    } catch {
+        if ($_.FullyQualifiedErrorId -match '^DC-') {
+            $Terminating_ErrorRecord = New-DefaultErrorRecord -InputObject $_
+            $PSCmdlet.ThrowTerminatingError($Terminating_ErrorRecord)
+        } else {
+            throw
+        }
+    }
+}
