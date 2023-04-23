91c2524a44367d16fca6b5b0f19b8f629cd686d6
Sean Quinlan <github@yster.org>
2023-04-23 20:25:44 +0100

    Test to trigger build

--- Source/Private/Shared_Variables.ps1
+++ Source/Private/Shared_Variables.ps1
@@ -1,7 +1,6 @@
 $Current_TimeZone = (Get-CimInstance -ClassName Win32_TimeZone).StandardName
 $TimeZone_Info = [System.TimeZoneInfo]::FindSystemTimeZoneById($Current_TimeZone)
 
-
 $Patch_Status_Mapping = @{
     'Installed' = 201
     'Missing'   = 202
